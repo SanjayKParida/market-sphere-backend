fa6dc622db3411795c75ee4759f027cffdc3ce4f
SanjayKParida <kumarparidasanjay23@gmail.com>
2025-01-16 21:23:28 +0530

    orderController updated

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -40,7 +40,7 @@ export const createOrder = async (req, res) => {
   }
 };
 
-export const fetchOrders = async (req, res) => {
+export const fetchBuyerOrders = async (req, res) => {
   try {
     //EXTRACT THE BUYER ID FROM THE REQUEST PARAMETER
     const { buyerId } = req.params;
@@ -53,6 +53,80 @@ export const fetchOrders = async (req, res) => {
         .json({ message: "No Orders found for this buyer." });
     } else {
       return res.status(200).json(orders);
+    }
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+export const deleteOrders = async (req, res) => {
+  try {
+    //EXTRACT THE ID FROM THE REQUEST TO DELETE
+    const { id } = req.params;
+    //FIND AND DELTE ORDER FROM DATABASE USING THE EXTRACTED ID
+    const deletedOrder = await Order.findByIdAndDelete(id);
+    //CHECK IF AN ORDER IS FOUND AND DELETED
+    if (!deletedOrder) {
+      //IF NO ORDER IS FOUND BY ID RETURN 404
+      res.status(404).json({ message: "Order not found" });
+    } else {
+      //IF ORDER WAS FOUND AND DELETED
+      res.status(200).json({ message: "Order deleted successfully" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const fetchVendorOrders = async (req, res) => {
+  try {
+    //EXTRACT THE BUYER ID FROM THE REQUEST PARAMETER
+    const { vendorId } = req.params;
+    //FIND ALL THE ORDERS IN THE DATABASE THAT MATCHES THE BUYER ID
+    const orders = await Order.find({ vendorId });
+    //IF NO ORDERS FOUND RETURN 404 STATUS
+    if (orders.length == 0) {
+      return res
+        .status(404)
+        .json({ message: "No Orders found for this vendor." });
+    } else {
+      return res.status(200).json(orders);
+    }
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+export const updateStatusDelivered = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updatedOrder = await Order.findByIdAndUpdate(
+      id,
+      { delivered: true },
+      { new: true }
+    );
+    if (!updatedOrder) {
+      res.status(404).json({ message: "Order not found" });
+    } else {
+      return res.status(200).json(updatedOrder);
+    }
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+export const updatedStatusProcessing = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updatedOrder = await Order.findByIdAndUpdate(
+      id,
+      { processing: false },
+      { new: true }
+    );
+    if (!updatedOrder) {
+      res.status(404).json({ message: "Order not found" });
+    } else {
+      return res.status(200).json(updatedOrder);
     }
   } catch (error) {
     return res.status(500).json({ message: error.message });
--- routes/order.js
+++ routes/order.js
@@ -1,10 +1,27 @@
 import express from "express";
-import { createOrder, fetchOrders } from "../controllers/orderController.js";
+import {
+  createOrder,
+  deleteOrders,
+  fetchBuyerOrders,
+  fetchVendorOrders,
+  updatedStatusProcessing,
+  updateStatusDelivered,
+} from "../controllers/orderController.js";
 
 export const orderRouter = express.Router();
 
 //POST ROUTE FOR CREATING ORDERS
 orderRouter.post("/api/orders", createOrder);
 
-//GET ROUTE FOR FETCHING ORDERS
-orderRouter.get("/api/orders/:buyerId", fetchOrders);
+//GET ROUTE FOR FETCHING ORDERS BY BUYER ID
+orderRouter.get("/api/orders/:buyerId", fetchBuyerOrders);
+
+//DELETE ROUTE FOR DELETING ORDERS
+orderRouter.delete("/api/orders/:id", deleteOrders);
+
+//GET ROUTE FOR FETCHING ORDERS BY VENDOR ID
+orderRouter.get("/api/orders/vendors/:vendorId", fetchVendorOrders);
+
+//PATCH ROUTE TO UPDATE DELIVERY STATUS
+orderRouter.patch("/api/orders/:id/delivered", updateStatusDelivered);
+orderRouter.patch("/api/orders/:id/processing", updatedStatusProcessing);
